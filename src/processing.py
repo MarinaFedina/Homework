a96fe1a77ad126970e0d22bfd17867896021cd96
Марина Федина <e.m.a_@mail.ru>
2025-06-18 23:18:20 +0300

    Создание функции filter_by_state

---
+++ src/processing.py
@@ -0,0 +1,25 @@
+def filter_by_state(set_of_lists:list[dict], state="EXECUTED"):
+    """Функция принимает на вход список словарей и возвращает новый список словарей,
+    содержащий только те словари, которые соответствуют указанному значению.
+    :return: list[dict]"""
+    new_list = []
+    for one_list in set_of_lists:
+        for key, value in one_list.items():
+            if key == "state" and value == "EXECUTED":
+                new_list.append(one_list)
+    return new_list
+
+
+test_list = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+
+#def sort_by_date():
+
+
+
+print(filter_by_state(test_list))
