cc479b7b6d8e055f4705073507d1e9de883b2621
Марина Федина <e.m.a_@mail.ru>
2025-06-26 12:50:32 +0300

    Завершила тест для первой функции в test_processing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 
-#Фикстуры.
+
+#Для тестов создаются фикстуры, которые предоставят тестовые данные.
 @pytest.fixture
 def card_num():
     return "7000792289606361"
@@ -12,6 +13,16 @@ def account_num():
 
 
 @pytest.fixture
+def state_executed_1():
+    return {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"}
+
+
+@pytest.fixture
+def state_executed_2():
+    return {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
+
+
+@pytest.fixture
 def num_empty():
     return " "
 
@@ -20,7 +31,6 @@ def num_empty():
 def msg_to_user():
     return "Введите номер"
 
-#Для всех тестов создайте фикстуры, которые предоставят тестовые данные
 # для списков словарей, включая различные случаи и комбинации state и date.
 #Покрытие тестами. Убедитесь, что все ветви кода и исключения,
 # которые могут быть сгенерированы вашими функциями, тестируются.
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,10 +1,25 @@
-#Функция
-#filter_by_state:
+import pytest
+from src.processing import filter_by_state
+from src.processing import sort_by_date
+
+
 #Тестирование фильтрации списка словарей по заданному статусу state.
+@pytest.mark.parametrize("original_list, new_list", [(
+    [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}],
+     [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
+)])
+def filter_by_state(original_list, new_list):
+    assert filter_by_state(original_list) == new_list
 #Проверка работы функции при отсутствии словарей с указанным статусом state в списке.
 #Параметризация тестов для различных возможных значений статуса state.
-#Функция
-#sort_by_date:
-#Тестирование сортировки списка словарей по датам в порядке убывания и возрастания.
+
+
+# Тестирование сортировки списка словарей по датам в порядке убывания и возрастания.
+def sort_by_date():
+    assert
 #Проверка корректности сортировки при одинаковых датах.
 #Тесты на работу функции с некорректными или нестандартными форматами дат.
