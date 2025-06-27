d27a62f9a09e17ed09d0c9a9116f765213e483f6
Марина Федина <e.m.a_@mail.ru>
2025-06-27 18:48:20 +0300

    masks fixed

--- src/masks.py
+++ src/masks.py
@@ -3,12 +3,19 @@ def get_mask_card_number(number: str) -> str:
     :return: str"""
     masked_number = []
     for symbol in number:
-        masked_number.append(symbol)
-    masked_number[6:-4] = "******"
-    masked_number.insert(4, " ")
-    masked_number.insert(9, " ")
-    masked_number.insert(14, " ")
-    return "".join(masked_number)
+        if not symbol.isdigit():
+            continue
+        else:
+            masked_number.append(symbol)
+        masked_number[6:-4] = "******"
+        masked_number.insert(4, " ")
+        masked_number.insert(9, " ")
+        masked_number.insert(14, " ")
+    user_msg = "Введите номер"
+    if len(masked_number) < 19:
+        print(user_msg)
+    else:
+        return "".join(masked_number)
 
 
 def get_mask_account(account_number: str) -> str:
@@ -17,3 +24,6 @@ def get_mask_account(account_number: str) -> str:
     last_numbers = "".join(account_number[-4:])
     masked_account = "**" + last_numbers
     return masked_account
+
+
+print(get_mask_card_number(""))
